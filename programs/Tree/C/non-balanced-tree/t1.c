9d583a8a685a3d514f2dd36d258ccca8fff3200c
Jairo Júnior <jairojjr2003@gmail.com>
2025-02-23 23:09:37 -0300

    .

--- programs/Tree/C/non-balanced-tree/t1.c
+++ programs/Tree/C/non-balanced-tree/t1.c
@@ -7,11 +7,11 @@ int main(){
     No* head = initTree(10);
 
     appendValue(head, 2);
-
     appendValue(head, 12);
     appendValue(head, 5);
     appendValue(head, 1);
     appendValue(head, 3);
+
 
     printValues(head);
     return 0;
